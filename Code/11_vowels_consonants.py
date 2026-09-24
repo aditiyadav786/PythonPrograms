50c7d52a8f0b00e18beea594872367c8eeec17b9
Aditi Yadav <aditiyadav786@gmail.com>
2026-09-24 18:09:00 +0530

    Add vowels cosonants program

---
+++ Code/11_vowels_consonants.py
@@ -0,0 +1,12 @@
+def count_vowels_consonants(text):
+    vowels = 0
+    consonants = 0
+
+    for char in text.lower():
+        if char.isalpha():
+            if char in "aeiou":
+                vowels += 1
+            else:
+                consonants += 1
+
+    return vowels, consonants

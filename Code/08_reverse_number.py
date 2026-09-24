92e5f0cce347663bb448f51f0755b711e594e98a
Aditi Yadav <aditiyadav786@gmail.com>
2026-09-24 17:43:47 +0530

    Add reverse program

---
+++ Code/08_reverse_number.py
@@ -0,0 +1,12 @@
+def reverse_number(n):
+    sign = -1 if n < 0 else 1
+    n = abs(n)
+
+    reversed_num = 0
+
+    while n > 0:
+        digit = n % 10
+        reversed_num = reversed_num * 10 + digit
+        n //= 10
+
+    return sign * reversed_num

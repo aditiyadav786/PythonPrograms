78e9878949922908d6615895b688a2c96164095d
Aditi Yadav <aditiyadav786@gmail.com>
2026-09-24 17:52:01 +0530

    Add palindrome program

---
+++ Code/09_palindrome_number.py
@@ -0,0 +1,10 @@
+def is_palindrome_number(n):
+    original = n
+    reversed_num = 0
+
+    while n > 0:
+        digit = n % 10
+        reversed_num = reversed_num * 10 + digit
+        n //= 10
+
+    return original == reversed_num
